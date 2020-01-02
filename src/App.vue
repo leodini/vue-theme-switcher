4d6907401c9e6aff4501e3c7456509d220bfb31f
leodini <leodini@hotmail.com>
2020-01-01 23:18:52 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app" :class="(mode === 'dark') ? 'dark' : ''">
+    <Header :mode="mode" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/components/Header'
 
 export default {
   name: 'app',
+  data() {
+    return{
+      mode: 'dark'
+    }
+  },
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  .app{
+    width: 100vw;
+    height: 100vh;
+    background: #F3F3F3;
+    color: #15202b;
+  }
+
+  .dark{
+    background: #192734;
+    color: #F3F3F3;
+  }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+    <header>
+        <h1>{{ mode }} Mode</h1>
+        <!-- Toggle -->
+        <nav>
+            <div class="nav-link">Home</div>
+            <div class="nav-link">About</div>
+            <div class="nav-link">Portfolio</div>
+            <div class="nav-link">Contact</div>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    props: ['mode']
+}
+</script>
+
+<style>
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0px 25px;
+        height: 60px;
+        background: #c3c3c3;
+        transition: background 0.3s ease-in-out;
+    }
+
+    .dark header{
+        background: #15202b;
+    }
+
+    header h1{
+        font-size: 32px;
+        text-transform: uppercase;
+        font-weight: 900;
+    }
+
+    header nav{
+        display: flex;
+        height: 60px;
+    }
+
+    header nav .nav-link{
+        display: flex;
+        align-items: center;
+        padding: 0px 15px;
+    }
+
+    header nav .nav-link:hover{
+        background: #b0b0b0;
+    }
+
+    .dark header nav .nav-link:hover{
+        background: #101520;
+    }
+</style>
